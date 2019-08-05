0b3a4af444264ba4f289d58924a071a77be8506d
Adityafr <adityapatwa.ap@gmail.com>
2019-08-05 10:04:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/posttest/LayoutScore.java
@@ -0,0 +1,14 @@
+package com.example.posttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_layout_score);
+    }
+}
